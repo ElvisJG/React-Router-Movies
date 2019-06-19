446bbc2941969e3deba03f4eacaca8c4448d0429
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-06-19 19:38:00 -0400

    stretch

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import MovieCard from './MovieCard';
 
 export default class MovieList extends Component {
   constructor(props) {
@@ -33,25 +34,6 @@ export default class MovieList extends Component {
 }
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
-    <Link to={`/movies/${movie.id}`}>
-      <div className='movie-card' key={movie.id}>
-        <h2>{title}</h2>
-        <div className='movie-director'>
-          Director: <em>{director}</em>
-        </div>
-        <div className='movie-metascore'>
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className='movie-star'>
-            {star}
-          </div>
-        ))}
-      </div>
-    </Link>
-  );
+  // const { title, director, metascore, stars } = movie;
+  return <MovieCard />;
 }
